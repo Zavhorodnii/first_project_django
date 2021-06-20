707a313756e9e4b43d3e93a9e5fb72674cd7dcdd
unknown <you@example.com>
2021-06-20 10:19:11 +0300

    lesson 7

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.MovieView.as_view())
+    path("", views.MovieView.as_view()),
+    path("<slug:slug>/", views.MovieDetailView.as_view(), name="movie_detail"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MovieView(View):
+class MovieView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = "movies/movies.html"
+    # def get(self, request):
+    #     movies = Movie.objects.all()
+    #     return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
+    # def get(self, request, slug):
+    #     movie = Movie.objects.get(url=slug)
+    #     return render(request, "movies/movie_detail.html", {"movie": movie})
