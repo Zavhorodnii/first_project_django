ef0342bbd21618103878092fc99a0b66b3a676d8
unknown <you@example.com>
2021-06-25 09:33:39 +0300

    local postgres

--- first_project/settings.py
+++ first_project/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_movie',
-        'USER': 'demo',
+        'USER': 'postgres',
         'HOST': 'localhost',
         'PORT': 5432,
-        'PASSWORD':'demo'
+        'PASSWORD':'root'
     }
 }
 
